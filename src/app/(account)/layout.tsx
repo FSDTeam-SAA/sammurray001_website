24ec20654d5bfe27e146270f2f45712ea59055a2
Tanvirgit07 <ahmmedtanvir009@gmail.com>
2025-11-15 16:09:38 +0600

    new add

--- src/app/(account)/layout.tsx
+++ src/app/(account)/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/web/header'
 import React from 'react'
 // import { Toaster } from 'sonner'
 
@@ -6,6 +7,7 @@ function layout({children}: {children: React.ReactNode}) {
     <div className='bg-[#0F172A]'>
       
       {/* <Toaster richColors  position="bottom-right"/> */}
+      <Header />
         {children}
     </div>
   )
